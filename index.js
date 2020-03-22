7dbb49168862ee83888c50f9cf24825ac5418b37
stevestring <stevestring@gmail.com>
2020-03-22 12:48:28 -0400

    Fabricated United States Element using state data

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/cs
 module.exports.handler = (event, context, callback) => {
         let response;
         // To debug your problem
-
+        var usJson = {"Province/State":"United States","Country/Region":"US","Lat":"47.4009","Long":"-121.4905","TimeSeries":{"1/22/20":"0","1/23/20":"0","1/24/20":"0","1/25/20":"0","1/26/20":"0","1/27/20":"0"}};
         https.get(url, function(res) {
             var body = '';
         
@@ -18,28 +18,36 @@ module.exports.handler = (event, context, callback) => {
             res.on('end', function() {
               var data = body;
 
-                //console.log(data);
-                
                 var newString =  data.toString().replace(/([0-2][0-9]|[0-9])[\/]([0-3][0-9]|[0-9])/g, 
                 function (x) {
                     return 'TimeSeries.'+x;
                 }
                 )        
-                //console.log(newString); 
 
                 csv({
                     output: "json"
                 })
                 .fromString(newString)
                 .then((jsonObj)=>{
-                    //console.log(jsonObj);
-
                     //Only US states
                     var filtered = jsonObj.filter(a=>a["Country/Region"] === "US"
                     && a["Province/State"].search(",") === -1);
 
-                    
-
+                    //Create USA element by adding up all states
+                    filtered.forEach(function(item) {
+                      for (var key in item.TimeSeries) {
+                        if (item.TimeSeries.hasOwnProperty(key)) {
+                          if (usJson.TimeSeries.hasOwnProperty(key)) {                          
+                            usJson.TimeSeries[key] = parseInt(usJson.TimeSeries[key])+ parseInt(item.TimeSeries[key]);
+                          }
+                          else
+                          {
+                            usJson.TimeSeries[key] = parseInt(item.TimeSeries[key]);
+                          }
+                        }
+                      }
+                      });
+                    filtered.push(usJson);
                     response = {
                             statusCode: 200,
                             headers: {
@@ -48,15 +56,8 @@ module.exports.handler = (event, context, callback) => {
                             },
                             body: JSON.stringify(filtered)                   
                             };
-                    //response = jsonObj;
                     context.succeed(response);
-                    //callback(null, response);
-                });
-        
-                //return response;
-                //console.log("response: " + JSON.stringify(response));
-            
-            
+                });                      
             
     });
     }).on('error', function(err) {
