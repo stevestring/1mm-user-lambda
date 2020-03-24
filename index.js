2d1c86df76aa02d8122bf101d98514c570208ba2
stevestring <stevestring@gmail.com>
2020-03-24 19:21:04 -0400

    new datasource

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ var csv = require('csvtojson');
 let fs = require("fs");
 let path = require("path");
 const https = require("https");
-const url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv';
+const url = 'https://www.bing.com/covid/graphdata';
 
 module.exports.handler = (event, context, callback) => {
         let response;
         // To debug your problem
-        var usJson = {"Province/State":"United States","Country/Region":"US","Lat":"47.4009","Long":"-121.4905","TimeSeries":{"1/22/20":"0","1/23/20":"0","1/24/20":"0","1/25/20":"0","1/26/20":"0","1/27/20":"0"}};
+        //var usJson = {"Province/State":"United States","Country/Region":"US","Lat":"47.4009","Long":"-121.4905","TimeSeries":{"1/22/20":"0","1/23/20":"0","1/24/20":"0","1/25/20":"0","1/26/20":"0","1/27/20":"0"}};
+        
+        
         https.get(url, function(res) {
             var body = '';
         
@@ -16,50 +18,28 @@ module.exports.handler = (event, context, callback) => {
             });
         
             res.on('end', function() {
-              var data = body;
-
-                var newString =  data.toString().replace(/([0-2][0-9]|[0-9])[\/]([0-3][0-9]|[0-9])/g, 
-                function (x) {
-                    return 'TimeSeries.'+x;
-                }
-                )        
-
-                csv({
-                    output: "json"
-                })
-                .fromString(newString)
-                .then((jsonObj)=>{
-                    //Only US states
-                    var filtered = jsonObj.filter(a=>a["Country/Region"] === "US"
-                    && a["Province/State"].search(",") === -1);
-
-                    //Create USA element by adding up all states
-                    filtered.forEach(function(item) {
-                      for (var key in item.TimeSeries) {
-                        if (item.TimeSeries.hasOwnProperty(key)) {
-                          if (usJson.TimeSeries.hasOwnProperty(key)) {                          
-                            usJson.TimeSeries[key] = parseInt(usJson.TimeSeries[key])+ parseInt(item.TimeSeries[key]);
-                          }
-                          else
-                          {
-                            usJson.TimeSeries[key] = parseInt(item.TimeSeries[key]);
-                          }
-                        }
-                      }
-                      });
-                    filtered.push(usJson);
-                    response = {
-                            statusCode: 200,
-                            headers: {
-                                'Content-Type': 'application/json',
-                                'Access-Control-Allow-Origin': '*' 
-                            },
-                            body: JSON.stringify(filtered)                   
-                            };
-                    context.succeed(response);
-                });                      
+              var data = JSON.parse(body);             
+              for (var key in data) {               
+                //console.log(key + " -> " + data1[0].TimeSeries[key]);
+                if (key.search("houstoncounty_alabama_unitedstates") ==-1 )
+                {
+                  //console.log("delete");
+                  delete data[key];
+                }    
+              } 
+              
+              response = {
+                statusCode: 200,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*' 
+                },
+                body: JSON.stringify(data)                   
+                };
+                context.succeed(response);
+              });                      
             
-    });
+
     }).on('error', function(err) {
       console.log("Something went wrong");
       console.log(err);
