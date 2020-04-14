b08f42a0c924252b95d4550f08a247722f42be36
stevestring <stevestring@gmail.com>
2020-04-14 19:15:16 -0400

    Added New User Test

--- test-event.js
+++ test-event.js
@@ -1,8 +1,10 @@
 module.exports = {
     "user1":"NeurotypicalPanda",
-    "user":"spy400qqq300",
+    "user1":"spy400qqq300",
     "key2": "FirangiPan",
     "key3": "aaasssaa",
-    "user1": "Forget-Hype"
+    "user1": "Forget-Hype",
+    "user":"Power80770M"
+
     
   };
